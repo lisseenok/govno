4bb766d3765855b3cb6ebc160ab8b30a89033fff
lisseenok <lisenok220503@yandex.ru>
2024-06-13 22:45:18 +0300

    fix

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,5 @@
 package org.example;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.formula.functions.Column;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -14,98 +9,115 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.util.Date;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
 
         try {
+            // путь к исходному файла
+            var sourceFilePath = "/Users/elizavetacaplina/IdeaProjects/mirea/govno/src/main/resources/аудиторный фонд обход_ЕВ.xlsx";
+            // путь к файлу с результатом
+            var resultFilePath = "/Users/elizavetacaplina/IdeaProjects/mirea/govno/src/main/resources/result/govno.xlsx";
+
             // Чтение файла
-            FileInputStream file = new FileInputStream(new File("C:\\Users\\etezova_fm\\IdeaProjects\\EBUCHI\\src\\main\\resources\\аудиторный фонд обход_ЕВ.xlsx"));
+            FileInputStream file = new FileInputStream(sourceFilePath);
             Workbook workbook = new XSSFWorkbook(file);
 
             // Создаем новую книгу для записи результата
             Workbook transposedWorkbook = new XSSFWorkbook();
             Sheet transposedSheet = transposedWorkbook.createSheet("Transposed Data");
 
+            // цикл по листам
             for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+                // получили лист
                 var sheet = workbook.getSheetAt(i);
 
-                var name = workbook.getSheetName(i);
+                // получили имя листа
+                var name = sheet.getSheetName();
+
+                // пропускаем ненужные листы
                 if ("Кафедральный аудит".equals(name) || "Перечень характеристик".equals(name)) continue;
+
+                // пишем имя листа
                 transposedSheet.createRow(i).createCell(0).setCellValue(name);
 
-                for (Row row: sheet) {
+                // создаем переменную для поиска заполненной колонки
+                int columnNum = -1;
+
+                // для каждой строки
+                for (Row row : sheet) {
+                    // пропускаем строки до 6-ой (с 0)
                     if (row.getRowNum() < 6) continue;
+
+                    // для каждой ячейке в строке
                     for (Cell cell : row) {
                         if (cell == null) continue;
+
+                        // пропускаем столбцы
                         if (cell.getColumnIndex() < 3 || cell.getColumnIndex() > 8) continue;
 
+                        // получаем значение
+                        var cellValue = getCellValue(cell);
 
-                        int columnNum = cell.getColumnIndex();
+                        // если значение есть
+                        if (cellValue != null && !cellValue.isBlank()) {
+                            // сохраняем в нашу переменную номер колонки
+                            columnNum = cell.getColumnIndex();
 
-                        var type = cell.getCellType();
-
-                        if (type == 0) {
-                            if (transposedSheet.getRow(i) == null) {
-                                transposedSheet.createRow(i).createCell(row.getRowNum()).setCellValue(cell.getNumericCellValue());
-                            } else {
-                                transposedSheet.getRow(i).createCell(row.getRowNum()).setCellValue(cell.getNumericCellValue());
-                            }
-                        } else if (type == 1) {
-                            if (transposedSheet.getRow(i) == null) {
-                                var val = cell.getStringCellValue();
-                                if (val != null)
-                                    transposedSheet.createRow(i).createCell(row.getRowNum()).setCellValue(cell.getStringCellValue());
-                            } else {
-                                var val = cell.getStringCellValue();
-                                if (val != null)
-                                    transposedSheet.getRow(i).createCell(row.getRowNum()).setCellValue(cell.getStringCellValue());
-                            }
+                            // пишем в результат файл
+                            if (transposedSheet.getRow(i) == null)
+                                transposedSheet.createRow(i).createCell(row.getRowNum()).setCellValue(cellValue);
+                            else
+                                transposedSheet.getRow(i).createCell(row.getRowNum()).setCellValue(cellValue);
                         }
-
-                        transposedSheet.getRow(i).createCell(5).setCellValue(sheet.getRow(5).getCell(columnNum).getStringCellValue());
                     }
                 }
+
+                // если была найдена заполненная колонка - пишем ее заголовок в строку
+                if (columnNum != -1)
+                    transposedSheet.getRow(i).createCell(5).setCellValue(sheet.getRow(5).getCell(columnNum).getStringCellValue());
             }
-
-
-
-            // Транспонируем данные
-//            for (int r = 0; r < sheet.getLastRowNum() + 1; r++) {
-//                Row row = sheet.getRow(r);
-//                for (int c = 0; c < row.getLastCellNum(); c++) {
-//                    Cell cell = row.getCell(c);
-//                    if (cell == null) continue;
-//
-//                    var type = cell.getCellType();
-//                    if (type == 0) {
-//                        if (transposedSheet.getRow(c) == null) {
-//                            transposedSheet.createRow(c).createCell(r).setCellValue(cell.getNumericCellValue());
-//                        } else {
-//                            transposedSheet.getRow(c).createCell(r).setCellValue(cell.getNumericCellValue());
-//                        }
-//                    } else if (type ==1) {
-//                        if (transposedSheet.getRow(c) == null) {
-//                            transposedSheet.createRow(c).createCell(r).setCellValue(cell.getStringCellValue());
-//                        } else {
-//                            transposedSheet.getRow(c).createCell(r).setCellValue(cell.getStringCellValue());
-//                        }
-//                    }
-//                }
-//            }
 
             // Сохраняем результат в новый файл
-            String desktopPath = "C:\\Users\\etezova_fm\\IdeaProjects\\EBUCHI\\src\\main\\resources\\result\\govno.xlsx";
-            try (FileOutputStream fileOut = new FileOutputStream(desktopPath)) {
+            try (FileOutputStream fileOut = new FileOutputStream(resultFilePath)) {
                 transposedWorkbook.write(fileOut);
             }
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    /**
+     * Функция для получения значения ячейки
+     */
+    public static String getCellValue(Cell cell) {
+        if (cell != null) {
+            switch (cell.getCellType()) {
+                case STRING:
+                    return cell.getRichStringCellValue().getString().trim();
+                case NUMERIC:
+                    double cellDouble = Math.abs(cell.getNumericCellValue());
+                    double floorDouble = Math.floor(cellDouble);
+                    if (cellDouble == floorDouble) {
+                        return Long.toString(Math.round(cell.getNumericCellValue()));
+                    } else {
+                        return Double.toString(cell.getNumericCellValue());
+                    }
+                case FORMULA:
+                    try {
+                        return cell.getRichStringCellValue().toString().trim();
+                    } catch (Exception e) {
+                        try {
+                            return Double.toString(cell.getNumericCellValue());
+                        } catch (Exception e1) {
+                            return null;
+                        }
+                    }
+                default:
+                    return null;
+            }
+        }
+        return null;
     }
 }
